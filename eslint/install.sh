83df0fc3504d32f1fc3203f4361895318c3d8502
Jason Dentler <jasondentler@gmail.com>
2017-11-30 17:34:09 -0600

    install mocha

--- eslint/install.sh
+++ eslint/install.sh
@@ -1,4 +1,5 @@
 if test ! $(which eslint)
+then
     sudo npm install eslint -g
     sudo npm install eslint-plugin-node -g
     sudo npm install babel-eslint -g
--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,7 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+if test ! $(which mocha)
+then
+  sudo npm install mocha -g
+fi
